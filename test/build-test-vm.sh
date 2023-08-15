182d090ab099471915f0d7cd8c18037f7bb21f18
Guanyu Wu <lambda@funcptr.org>
2023-08-14 22:05:33 -0400

    Build ESP for UKI testing

--- test/build-test-vm.sh
+++ test/build-test-vm.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+die() {
+    exit 1;
+}
+
 cd $(dirname $0)/..
 
 mkdir -p test/.pkgcache
 
-podman run --privileged -v $(pwd)/src:/opt/bin:ro -v $(pwd)/test:/config:ro -v $(pwd)/test:/output -v $(pwd)/test/.pkgcache:/etc/apk/cache -it funcptr/summitkit
+podman run --privileged -v $(pwd)/src:/opt/bin:ro -v $(pwd)/test:/config:ro -v $(pwd)/test:/output -v $(pwd)/test/.pkgcache:/etc/apk/cache -it funcptr/summitkit || die
+
+pushd test > /dev/null
+
+if ! [ -e "esp.fat" ]; then
+  touch esp.fat
+  fallocate -z -l 256M esp.fat
+  mformat -i esp.fat ::
+fi
+
+mcopy -no -s -i esp.fat EFI ::
+mcopy -no -i esp.fat uki.efi ::/EFI/boot/bootx64.efi
+
+popd > /dev/null
