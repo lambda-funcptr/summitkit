ea880e386f9082f81a270fbd4ed852a46f248a4d
Guanyu Wu <lambda@funcptr.org>
2023-08-14 21:16:21 -0400

    Bind to /etc/apk/cache to allow caching

--- test/build-test-vm.sh
+++ test/build-test-vm.sh
@@ -2,4 +2,6 @@
 
 cd $(dirname $0)/..
 
-podman run --privileged -v $(pwd)/src:/opt/bin:ro -v $(pwd)/test:/config:ro -v $(pwd)/test:/output -it funcptr/summitkit
+mkdir -p test/.pkgcache
+
+podman run --privileged -v $(pwd)/src:/opt/bin:ro -v $(pwd)/test:/config:ro -v $(pwd)/test:/output -v $(pwd)/test/.pkgcache:/etc/apk/cache -it funcptr/summitkit
